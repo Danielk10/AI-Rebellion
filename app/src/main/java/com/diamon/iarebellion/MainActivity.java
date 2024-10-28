fe13b06792c18b69c23a1330206155cb3143e5c1
Danielk10 <danielpdiamon@gmail.com>
2024-10-28 19:39:02 -0400

    Configuración

--- app/src/main/java/com/diamon/iarebellion/MainActivity.java
+++ app/src/main/java/com/diamon/iarebellion/MainActivity.java
@@ -1,6 +1,10 @@
 package com.diamon.iarebellion;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View;
+import android.view.inputmethod.CompletionInfo;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(
+                            systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                    return insets;
+                });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu arg0, View arg1, ContextMenuInfo arg2) {
+        super.onCreateContextMenu(arg0, arg1, arg2);
+        // TODO: Implement this method
     }
 }
